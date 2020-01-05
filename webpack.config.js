d4420cdfb1c7759458d81db775d675cff77e40ba
Vasyl Savitskyy <33401147+qPrutp@users.noreply.github.com>
2020-01-06 00:34:52 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const PATHS = {
     src: path.join(__dirname, './src'),
@@ -73,6 +74,7 @@ module.exports = {
         port: 8081
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             hash: false,
             template: `${PATHS.src}/index.html`,
